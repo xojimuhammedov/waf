91ad5225432b59b8ce90ae961784e2fea19a83fb
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2025-01-02 18:27:27 +0500

    add

--- src/pages/DashboardPage/_components/BottomComponent.tsx
+++ src/pages/DashboardPage/_components/BottomComponent.tsx
@@ -29,18 +29,36 @@ function BottomComponent() {
         <p style={{ color: '#A3A3A3' }} className="mb-4 text-lg font-medium">
           TOP Hujumchi davlatlar
         </p>
-        {countryData?.map((item, index) => (
-          <div key={index} className="mb-2 flex items-center justify-between">
-            <p
-              style={{ width: item?.process }}
-              className={
-                'bottom-item flex h-[32px] cursor-pointer items-center gap-2 rounded px-2 text-sm text-white'
-              }>
-              <img src={item.img} className="h-6 w-6 rounded-full object-cover" alt="" />
+        {countryData?.map((item: any, index) => (
+          <div key={index} className="bottom-card relative mb-2 flex items-center justify-between">
+            <p className="bottom-items flex h-[32px] cursor-pointer items-center gap-2 px-2 text-sm text-white">
+              <img
+                src={item.img}
+                className="bottom-items h-6 w-6 rounded-full object-cover"
+                alt=""
+              />
               {item?.title}
             </p>
+            <div
+              style={{ width: `${item?.count / 10}%` }}
+              className="bottom-item cursor-pointer rounded"></div>
             <p className="text-sm text-white">{item?.count}</p>
           </div>
+          // <div key={index} className="bottom-card relative mb-2 flex items-center justify-between">
+          //   <p
+          //     className={
+          //       ' flex h-[32px] cursor-pointer items-center gap-2 rounded px-2 text-sm text-white'
+          //     }>
+          // <img
+          //   src={item.img}
+          //   className="bottom-items h-6 w-6 rounded-full object-cover"
+          //   alt=""
+          // />
+          //     <span className="bottom-items">{item?.title}</span>
+          //   </p>
+          //   <div style={{ width: `${item?.process / 10}%` }} className="bottom-item"></div>
+          //   <p className="text-sm text-white">{item?.count}</p>
+          // </div>
         ))}
       </div>
       <div className="w-1/2">
@@ -48,13 +66,14 @@ function BottomComponent() {
           TOP Hujum turlari
         </p>
         {get(data, 'data')?.map((item: any, index: number) => (
-          <div key={index} className="mb-2 flex items-center justify-between">
-            <p
-              // style={{ width: item?.count }}
-              className="bottom-item flex h-[32px] cursor-pointer items-center gap-2 rounded px-2 text-sm text-white">
+          <div key={index} className="bottom-card relative mb-2 flex items-center justify-between">
+            <p className="bottom-items flex h-[32px] cursor-pointer items-center gap-2 px-2 text-sm text-white">
               <HujumIcon />
               {item?.type}
             </p>
+            <div
+              style={{ width: `${item?.count / 10}%` }}
+              className="bottom-item cursor-pointer rounded"></div>
             <p className="text-sm text-white">{item?.count}</p>
           </div>
         ))}
